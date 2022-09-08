33b1936d327c35e248cadda44544abe76b246b76
alifaraji <alifaraji65452@gmail.com>
2022-09-09 01:49:40 +0430

    socket fixed-3

--- src/contexts/SocketProvider.tsx
+++ src/contexts/SocketProvider.tsx
@@ -8,11 +8,7 @@ export const socketContext = createContext<SocketContextInterface|null>(null)
 const SocketProvider = ({ id,children }:{id:string, children:React.ReactNode}) => {
   const [socket, setSocket] = useState()
   useEffect(() => {
-<<<<<<< HEAD
     const newSocket:any = io('https://bright-pike-blazer.cyclic.app:8080/', { query: { id } })
-=======
-    const newSocket:any = io('https://bright-pike-blazer.cyclic.app:8000/', { query: { id } })
->>>>>>> 12b47fe5ec49bfb2ce16b09637d7ba7da6f92f19
     setSocket(newSocket)
     return () => newSocket.close()
   }, [id])
